d3fbb332244e27eb12ee59097c5e6f3c7da8085c
RohanVB <rohan.vbh@gmail.com>
2019-06-04 12:43:28 -0400

    adds combine_final

---
+++ combine_final.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import os
+
+data_path = '/Users/rohan/Documents/Grad School/coursework/winter/ML & NLP/project_code/data/results/' \
+            'preprocessed_bloomberg.csv'
+file_path = '/Users/rohan/Documents/Grad School/Coursework/Winter/ML & NLP/Project_Code/data/company_prices'
+path_pth = ''.join([pth for pth, dirs, files in os.walk(file_path)])
+path_files = [files for pth, dirs, files in os.walk(file_path)]
+
+total_vals = [item for sublist in path_files for item in sublist]
+
+reuters_df = pd.read_csv(data_path)
+
+for i in total_vals:
+    try:
+        left = pd.DataFrame()
+        ticker_list = []
+        text_list = []
+        headlines_list = []
+        date_list = []
+
+        for j, k, l, m in zip(reuters_df['Ticker'], reuters_df['text'], reuters_df['headlines'], reuters_df['date']):
+            if j == i:
+                ticker_list.append(j)
+                text_list.append(k)
+                headlines_list.append(l)
+                date_list.append(m)
+
+        left['ticker'] = ticker_list
+        left['text'] = text_list
+        left['timestamp'] = date_list
+        left['headlines'] = headlines_list
+        left = left.dropna()
+        left = left.sort_values('timestamp')
+
+        right = pd.read_csv(path_pth + '/' + i)  # timestamp, ticker, signal
+        right = right.dropna()
+        right = right.sort_values('timestamp')
+
+        new_df = left.merge(right, on='timestamp', how='outer')
+        new_df = new_df.dropna()
+        # print(new_df)
+        new_df.to_csv('/Users/rohan/Documents/Grad School/coursework/winter/ML & NLP/'
+                      'project_code/data/results/bloomberg_task/' + i)
+    except KeyError:
+        print(i)
